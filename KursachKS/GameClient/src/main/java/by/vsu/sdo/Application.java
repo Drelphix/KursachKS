19cf6e3ba50f49810cb4371a2a862f17231be621
Drelphix <alexeydemeshko@gmail.com>
2019-06-16 16:10:34 +0300

    update
    NEW

--- KursachKS/GameClient/src/main/java/by/vsu/sdo/Application.java
+++ KursachKS/GameClient/src/main/java/by/vsu/sdo/Application.java
@@ -9,37 +9,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Application {
+    public int[] mas = new int[8];
     public static void main(String[] args) throws IOException {
 
-            System.out.println("Welcome to Client side");
-            System.out.println("Please input server ip");
-        Scanner input = new Scanner(System.in);
-        String ip = input.next();
-        System.out.println("Connecting to... "+ ip);
-            Socket fromserver = new Socket(ip,4444);
-            BufferedReader in  = new
-                    BufferedReader(new
-                    InputStreamReader(fromserver.getInputStream()));
-            PrintWriter    out = new
-                    PrintWriter(fromserver.getOutputStream(),true);
-            BufferedReader inu = new
-                    BufferedReader(new InputStreamReader(System.in));
-
-            String fuser,fserver;
-
-            while ((fuser = inu.readLine())!=null) {
-                out.println(fuser);
-                fserver = in.readLine();
-                System.out.println(fserver);
-                if (fuser.equalsIgnoreCase("close")) break;
-                if (fuser.equalsIgnoreCase("exit")) break;
-            }
-
-
-            out.close();
-            in.close();
-            input.close();
-            inu.close();
-            fromserver.close();
-        }
-    }
+    }}
---
+++ KursachKS/GameClient/src/main/java/by/vsu/sdo/checkers/ChessBoard.java
@@ -0,0 +1,125 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class ChessBoard extends JFrame implements MouseListener, MouseMotionListener {
+    public final static String WHITE_CHECKER = "./src/main/java/by/vsu/sdo/checkers/resourses/white.png";
+    public final static String BLACK_CHECKER = "./src/main/java/by/vsu/sdo/checkers/resourses/black.png";
+    public final static String WHITE_QUEEN = "./src/main/java/by/vsu/sdo/checkers/resourses/white_queen.png";
+    public final static String BLACK_QUEEN = "./src/main/java/by/vsu/sdo/checkers/resourses/black_queen.png";
+    private final static Color BLACK_COLOR = new Color(152, 89, 46);
+    private final static Color WHITE_COLOR = new Color(233, 216, 159);
+    JLayeredPane layeredPane;
+    JPanel chessBoard;
+    JLabel chessPiece;
+    int xAdjustment;
+    int yAdjustment;
+
+    public ChessBoard() {
+        Dimension boardSize = new Dimension(800, 800);
+        layeredPane = new JLayeredPane();
+        getContentPane().add(layeredPane);
+        layeredPane.setPreferredSize(boardSize);
+        layeredPane.addMouseListener(this);
+        layeredPane.addMouseMotionListener(this);
+
+
+        chessBoard = new JPanel();
+        layeredPane.add(chessBoard, JLayeredPane.DEFAULT_LAYER);
+        chessBoard.setLayout(new GridLayout(8, 8));
+        chessBoard.setPreferredSize(boardSize);
+
+
+        chessBoard.setBounds(0, 0, boardSize.width, boardSize.height);
+
+        for (int i = 0; i < 64; i++) {
+            JPanel square = new JPanel(new BorderLayout());
+            chessBoard.add(square);
+
+            int row = (i / 8) % 2;
+            if (row == 0)
+                square.setBackground(i % 2 == 0 ? WHITE_COLOR : BLACK_COLOR);
+
+            else
+                square.setBackground(i % 2 == 0 ? BLACK_COLOR : WHITE_COLOR);
+
+        }
+    }
+        public void PutCheckers (int[] mas){
+    JLabel piece;
+        for (int i:mas){
+            for (int j:mas){
+                if (mas[j]==1){
+                    piece = new JLabel(new ImageIcon(WHITE_CHECKER));
+                }   else if (mas[j]==2){
+                    piece = new JLabel(new ImageIcon(BLACK_CHECKER));
+                } else piece = null;
+                JPanel panel = (JPanel) chessBoard.getComponent(j);
+                panel.add(piece);
+                }
+            }
+        }
+
+
+    public static void main(String[] args) {
+        JFrame frame = new ChessBoard();
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void mousePressed(MouseEvent e) {
+        chessPiece = null;
+        Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+
+        if (c instanceof JPanel) return;
+
+        Point parentLocation = c.getParent().getLocation();
+        xAdjustment = parentLocation.x - e.getX();
+        yAdjustment = parentLocation.y - e.getY();
+        chessPiece = (JLabel) c;
+        chessPiece.setLocation(e.getX() + xAdjustment, e.getY() + yAdjustment);
+        chessPiece.setSize(chessPiece.getWidth(), chessPiece.getHeight());
+        layeredPane.add(chessPiece, JLayeredPane.DRAG_LAYER);
+    }
+
+    public void mouseDragged(MouseEvent me) {
+        if (chessPiece == null) return;
+
+        chessPiece.setLocation(me.getX() + xAdjustment, me.getY() + yAdjustment);
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        if (chessPiece == null) return;
+
+        chessPiece.setVisible(false);
+        Component c = chessBoard.findComponentAt(e.getX(), e.getY());
+
+        if (c instanceof JLabel) {
+            Container parent = c.getParent();
+            parent.remove(0);
+            parent.add(chessPiece);
+        } else {
+            Container parent = (Container) c;
+            parent.add(chessPiece);
+        }
+
+        chessPiece.setVisible(true);
+    }
+
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+}
