ad7663a053f6f3f0671ead8e7f67ef7b23c7c24b
Drelphix <alexeydemeshko@gmail.com>
2019-06-16 19:51:27 +0300

    BUILD 0.01

--- KursachKS/GameClient/src/main/java/by/vsu/sdo/checkers/Board.java
+++ KursachKS/GameClient/src/main/java/by/vsu/sdo/checkers/Board.java
@@ -2,35 +2,37 @@ package by.vsu.sdo.checkers;
 
 import javax.swing.*;
 import java.awt.*;
+
 public class Board {
+    public final static ImageIcon WHITE_CHECKER = new ImageIcon("./resourses/white.png");
+    public final static ImageIcon BLACK_CHECKER = new ImageIcon("./resourses/black.png");
+    public final static ImageIcon WHITE_QUEEN = new ImageIcon("./resourses/white_queen.png");
+    public final static ImageIcon BLACK_QUEEN = new ImageIcon("./resourses/black_queen.png");
     private final static Color BLACK_COLOR = new Color(152, 89, 46);
     private final static Color WHITE_COLOR = new Color(233, 216, 159);
-    public final static ImageIcon WHITE_CHECKER = new ImageIcon("./resourses/white.png");
-    public final static ImageIcon BLACK_CHECKER = new ImageIcon("E:\\KursachKS\\KursachKS\\GameClient\\resourses\\black.png");
-    public final static ImageIcon WHITE_QUEEN = new ImageIcon("./src/main/java/by/vsu/sdo/checkers/resourses/white_queen.png");
-    public final static ImageIcon BLACK_QUEEN = new ImageIcon("./src/main/java/by/vsu/sdo/checkers/resourses/black_queen.png");
-    private JLayeredPane layeredPane;
-    private JPanel chessBoard;
-    private JLabel checkers;
 
     public void DrawBoard() {
-    JFrame board = new JFrame("Checkers");
-    board.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    Dimension boardSize = new Dimension(800,800);
-    board.setVisible(true);
-    board.setResizable(false);
-    board.setPreferredSize(boardSize);
-    for (int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            JLabel label = new JLabel("Test label");
+        JFrame window = new JFrame("Checkers");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Dimension boardSize = new Dimension(800, 900);
+        window.setLayout(new GridLayout(8, 8));
+        window.setVisible(true);
+        window.setResizable(false);
+        window.setPreferredSize(boardSize);
+        for (int i = 0; i < 64; i++) {
+            JPanel square = new JPanel(new BorderLayout());
+            window.add(square);
+            int row = (i / 8) % 2;
+            if (row == 0)
+                square.setBackground(i % 2 == 0 ? WHITE_COLOR : BLACK_COLOR);
+            else
+                square.setBackground(i % 2 == 0 ? BLACK_COLOR : WHITE_COLOR);
         }
-    }
-    board.getContentPane().add(label);
-    board.pack();
-    board.setLocationRelativeTo(null);
+        window.pack();
+        window.setLocationRelativeTo(null);
     }
 
-    public void PutChekers(){
+    public void PutChekers() {
 
     }
 }
