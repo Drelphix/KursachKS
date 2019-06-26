570ffd7c57bd293a0b47f65e5be591b60830c0e3
FaustMont <slavyn21@gmail.com>
2019-06-26 18:57:07 +0300

    1
    1

--- KursachKS/CappucinoChat/src/sample/Connect.java
+++ KursachKS/CappucinoChat/src/sample/Connect.java
@@ -1,11 +1,15 @@
 package sample;
 
-public class Connect {
+import java.net.Socket;
+
+public class Connect{
+    private static final String HOST = "192.168.137.1"; // адрес сервера
+    private static final int PORT = 4444; // порт
+    private Socket clientSocket; // клиентский сокет
 
 
     public void Connecting(){
-        static final int PORT = 4444;
-
+        clientSocket = new Socket(HOST, PORT); // подключаемся к серверу
     }
 
     public void Close(){
